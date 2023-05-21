ae59f9be42c058baee25922130bcd442271cbe51
Ramkumar <manuthiyyakkatt@gmail.com>
2023-05-21 22:56:20 +0100

    cleanup

--- Planets/Planets/Data/Repositories/DefaultPlanetsRepository.swift
+++ Planets/Planets/Data/Repositories/DefaultPlanetsRepository.swift
@@ -13,39 +13,10 @@ final class DefaultPlanetsRepository {
     
     @Injected(\.dataTransferService) private var dataTransferService: DataTransferService
     @Injected(\.planetsResponseStorage)  private var cache: PlanetsResponseStorage
-    
-//    init(dataTransferService: DataTransferService, cache: PlanetsResponseStorage) {
-//        self.dataTransferService = dataTransferService
-//        self.cache = cache
-//    }
+
 }
 
 extension DefaultPlanetsRepository: PlanetsRepository {
-    
-//    public func fetchPlanets(query: PlanetsQueryModel,
-//                             cached: @escaping (PlanetsPage) -> Void,
-//                             completion: @escaping (Result<PlanetsPage, Error>) -> Void) -> Cancellable? {
-//        
-//        let requestDTO = PlanetsRequestDTO(page: query.page)
-//        let task = RepositoryTask()
-//        
-//        //            if case let .success(responseDTO?) = result {
-//        //                cached(responseDTO.toDomain())
-//        //            }
-//        //  guard !task.isCancelled else { return }
-//        
-//        let endpoint = APIEndpoints.getPlanetsList(with: requestDTO)
-//        task.networkTask = self.dataTransferService.request(with: endpoint) { result in
-//            switch result {
-//            case .success(let responseDTO):
-//                self.cache.save(response: responseDTO, for: requestDTO)
-//                completion(.success(responseDTO.toDomain()))
-//            case .failure(let error):
-//                completion(.failure(error))
-//            }
-//        }
-//        return task
-//    }
     
     public func fetchPlanets(query: PlanetsQueryModel) async throws -> PlanetsPage {
         let requestDTO = PlanetsRequestDTO(page: query.page)
@@ -64,13 +35,6 @@ extension DefaultPlanetsRepository: PlanetsRepository {
         } catch {
             throw error
         }
-//        let result =  await dataTransferService.request(with: endpoint)
-//        switch result {
-//        case .success(let responseDTO):
-//            self.cache.save(response: responseDTO, for: requestDTO)
-//            return responseDTO.toDomain()
-//        case .failure(let error):
-//            throw error
-//        }
+
     }
 }
